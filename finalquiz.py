0e97fd0b2bd0b54b693c976afbb06865cd4f9650
Dhanik2203 <64954379+Dhanik2203@users.noreply.github.com>
2020-06-12 11:40:42 +0530

    Update finalquiz.py

--- finalquiz.py
+++ finalquiz.py
@@ -4,28 +4,28 @@ score=0
 total_q=4
 if ans.lower() == 'yes':
     ans=input('1.What is Best Programming language: Option are:1:python,2:java')
-    if ans.lower() =='python':
+    if ans.lower() =='1':
         score +=1
         print('correct Answer')
     else:
         print('incorrect Answer')
 
     ans = input('2.Who is india Prime Minister:Option are:1:narendra Modi,2:Rahul Gandhi ')
-    if ans.lower()== 'narendra Modi' or ans=='modi':
+    if ans.lower()== '1':
         score += 1
         print('correct Answer')
     else:
         print('incorrect Answer')
 
     ans = input('3.What is 3+6+3 ? : Option are:1:12,2:11')
-    if ans == '12':
+    if ans == '1':
         score += 1
         print(' correct Answer')
     else:
         print('incorrect Answer')
 
     ans = input('4.which Year corona virus war introduce: Option are:1:2019,2:2020')
-    if ans == '2020':
+    if ans == '2':
         score += 1
         print('correct Answer')
     else:
@@ -34,7 +34,7 @@ if ans.lower() == 'yes':
     print('Thank for playing this game , You got',score,"question correct")
     mark=(score/total_q) * 100
     print("mark",mark)
+    print("All Questions Answer is:1=python,2=narendra modi,3=12,4=2020 ")
 
-print("All Questions Answer is:1=python,2=narendra modi,3=12,4=2020 ")
 print("good bye")
 
